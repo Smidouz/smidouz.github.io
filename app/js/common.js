3379d15eaf801c10f794be1f5857ac1819f7ce97
Smidouz <klabukovmaksim@gmail.com>
2017-01-24 11:39:52 +0200

    24.01.17

---
+++ app/js/common.js
@@ -0,0 +1,36 @@
+$(function() {
+
+	$(".toggle_menu").click(function() {
+		$(this).toggleClass("on");
+		$(".main_menu").slideToggle();
+	});
+
+	/*jQuery Simple Equal Heights*/
+	$('.four_option .option').equalHeights();
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
