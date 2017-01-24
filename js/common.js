6718b03375892e822a4102b0258c8b02609c05b4
Smidouz <klabukovmaksim@gmail.com>
2017-01-24 14:41:55 +0200

    Animated
    Добавил анимацию на шапку и секцию 1

--- js/common.js
+++ js/common.js
@@ -5,8 +5,53 @@ $(function() {
 		$(".main_menu").slideToggle();
 	});
 
+	/*Scroll Function*/
+	$("#Features").click(function(){
+		$("html, body").animate({ scrollTop: $(".creative_features").height()+500 }, "slow");
+	});
+
+	$(".header_sliders .explore_btn").click(function(){
+		$("html, body").animate({ scrollTop: $(".creative_features").height()+500 }, "slow");
+	});
+
+	/*CSS Animation Qjs*/
+	$(".header_sliders h1").animated("fadeInRight");
+	$(".header_sliders h2").animated("fadeInLeft");
+	$(".header_sliders h3").animated("fadeInLeft");
+	$(".header_sliders .explore_btn").animated("bounceInUp");
+	$(".header_sliders .purchase_btn").animated("bounceInUp");
+	$(".common_sect h2").animated("flipInX");
+	
+
+	/*SCRIPT START Одиночное появление с добавлениеm классов*/
+	$(".four_option").waypoint(function(){
+		$(".four_option .option").each(function(index) {
+			var ths = $(this);
+			setInterval(function(){
+					ths.addClass("on");		
+			}, 300*index);
+			
+		});
+		
+	}, {
+		offset : "500"
+	});
+
+	$(".features_items").waypoint(function(){
+		$(".features_items .features_item").each(function(index) {
+			var ths = $(this);
+			setInterval(function(){
+					ths.addClass("on");		
+			}, 150*index);
+			
+		});
+		
+	}, {
+		offset : "500"
+	});
+
 	/*jQuery Simple Equal Heights*/
-	$('.four_option .option').equalHeights();
+	// $('.four_option .option').equalHeights();
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -14,6 +59,12 @@ $(function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
+
+	/*Отмена событий перехода по ссылке*/
+	$(".main_menu a").click(function(){
+		event.preventDefault();
+		// alert ("Work")
+	});
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
