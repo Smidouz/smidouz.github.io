57b9c3bb3e3eb5840c08a6f1e32b701b60b9d01b
Smidouz <klabukovmaksim@gmail.com>
2017-01-24 16:21:15 +0200

    animation_on_mobail

--- js/common.js
+++ js/common.js
@@ -15,21 +15,38 @@ $(function() {
 	});
 
 	/*CSS Animation Qjs*/
-	// $(".header_sliders h1").animated("fadeInRight");
-	// $(".header_sliders h2").animated("fadeInLeft");
-	// $(".header_sliders h3").animated("fadeInLeft");
-	// $(".header_sliders .explore_btn").animated("bounceInUp");
-	// $(".header_sliders .purchase_btn").animated("bounceInUp");
+	$(".header_sliders h1").animated("fadeInRight");
+	$(".header_sliders h2").animated("fadeInLeft");
+	$(".header_sliders h3").animated("fadeInLeft");
+	$(".header_sliders .explore_btn").animated("bounceInUp");
+	$(".header_sliders .purchase_btn").animated("bounceInUp");
+	$(".four_option .option_1").animated("flipInX");
+	$(".four_option .option_2").animated("flipInX");
+	$(".four_option .option_3").animated("flipInX");
+	$(".four_option .option_4").animated("flipInX");
 	$(".common_sect h2").animated("flipInX");
 	
 
 	/*SCRIPT START Одиночное появление с добавлениеm классов*/
-	$(".four_option").waypoint(function(){
-		$(".four_option .option").each(function(index) {
+	// $(".four_option").waypoint(function(){
+	// 	$(".four_option .option").each(function(index) {
+	// 		var ths = $(this);
+	// 		setInterval(function(){
+	// 				ths.addClass("on");		
+	// 		}, 300*index);
+			
+	// 	});
+		
+	// }, {
+	// 	offset : "500"
+	// });
+
+	$(".features_items").waypoint(function(){
+		$(".features_items .features_item").each(function(index) {
 			var ths = $(this);
 			setInterval(function(){
 					ths.addClass("on");		
-			}, 300*index);
+			}, 250*index);
 			
 		});
 		
@@ -37,17 +54,14 @@ $(function() {
 		offset : "500"
 	});
 
-	$(".features_items").waypoint(function(){
-		$(".features_items .features_item").each(function(index) {
-			var ths = $(this);
-			setInterval(function(){
-					ths.addClass("on");		
-			}, 150*index);
-			
-		});
-		
-	}, {
-		offset : "500"
+	/*Карусель Слайдов*/
+	$("").owlCarousel({
+		items : 1,
+		loop : true,
+		autoplaySpeed : 1200,
+		navSpeed : 1200,
+		dotsSpeed : 1200,
+		dragEndSpeed : 1200
 	});
 
 	/*jQuery Simple Equal Heights*/
