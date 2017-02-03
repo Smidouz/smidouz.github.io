2fe3cf2cee7a0c8a89ea6ca4f36b650905e0a1cb
Smidouz <klabukovmaksim@gmail.com>
2017-02-03 22:09:32 +0200

    v1

--- js/common.js
+++ js/common.js
@@ -1,65 +1,40 @@
 $(function() {
-
-	$(".toggle_menu").click(function() {
+	/*Script Menu-------------------------*/
+	$("#toggle").click(function() {
 		$(this).toggleClass("on");
-		$(".main_menu").slideToggle();
+		$("#menu").toggleClass("on");
+		$(".toggle_wrap").toggleClass("on");
 	});
+	/*------------------- Finish Script Menu*/
 
-	/*Scroll Function*/
-
-	// $(".header_sliders .explore_btn").click(function(){
-	// 	$("html, body").animate({ scrollTop: $(".creative_features").height()+500 }, "slow");
-	// });
-
-	/*CSS Animation Qjs*/
-	$(".header_sliders h1").animated("fadeInLeft");
-	$(".header_sliders h2").animated("fadeInRight");
-	$(".header_sliders h3").animated("fadeInRight");
-	$(".header_sliders .explore_btn").animated("fadeInUpBig");
-	$(".header_sliders .purchase_btn").animated("fadeInUpBig");
-	$(".four_option .option_1").animated("zoomIn");
-	$(".four_option .option_2").animated("zoomIn");
-	$(".four_option .option_3").animated("zoomIn");
-	$(".four_option .option_4").animated("zoomIn");
-	$(".section_service").animated("zoomInUp");
-	
-
-	/*SCRIPT START Одиночное появление с добавлениеm классов*/
-
-	$(".features_items").waypoint(function(){
-		$(".features_items .features_item").each(function(index) {
-			var ths = $(this);
-			setInterval(function(){
-					ths.addClass("on");		
-			}, 250*index);
-			
-		});
-		
-	}, {
-		offset : "500"
-	});
-
-	/*Карусель Слайдов*/
-	$(".service_slider").owlCarousel({
-		items : 1,
+	/*Owl Carousel ------------------------*/
+  $(".about_me_slider").owlCarousel({
+  	items : 1,
+  	nav : true,
 		URLhashListener:true,
 		autoplaySpeed : 1200,
 		navSpeed : 1200,
 		dotsSpeed : 1200,
 		dragEndSpeed : 1200
+  });
+  /*Script Scrol Anchor -------------------*/
+  $('a[href*="#"]:not([href="#"])').click(function() {
+  	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    	var target = $(this.hash);
+    	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    	if (target.length) {
+      	$('html, body').animate({
+        	scrollTop: target.offset().top
+      	}, 1000);
+      	return false;
+    	}
+  	}
 	});
 
-	$(".work_slider").owlCarousel({
-		items : 1,
-		nav : true,
-		autoplaySpeed : 1200,
-		navSpeed : 1200,
-		dotsSpeed : 1200,
-		dragEndSpeed : 1200
-	});
+  /*Finished Script Scrol Anchor ------------------*/
 
-	/*jQuery Simple Equal Heights*/
-	$(".service_menu a").equalHeights();
+	/*Animate CSS*/
+	$(".mouse_scroll ").animated("fadeInDown");
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -68,53 +43,9 @@ $(function() {
 		});
 	};
 
-	/*Отмена событий перехода по ссылке*/
-	// $(".main_menu a").click(function(){
-	// 	event.preventDefault();
-	// 	// alert ("Work")
-	// });
-
-	// $(".button_slider a").click(function(){
-	// 	event.preventDefault();
-	// 	// alert ("Work")
-	// });
-
-	$(".work_menu_wrap a").click(function(){
-		event.preventDefault();
-		// alert ("Work")
-	});
-
-	$(".work_slid a").click(function(){
-		event.preventDefault();
-		// alert ("Work")
-	});
-
-	/*Gallery MagnificPopup*/
-	$(".work_slid_img").magnificPopup({
-  	type: 'image',
-  	gallery:{
-    	enabled:true
-  	}
-	});
-	/*Form MagnificPopup*/
-	$(".purchase_btn").magnificPopup({
-  	type: 'inline',
-  	mainClass: 'mfp-forms'
-	});
-
-
-	/*для всех якорей на сайте, плавный переход*/
-	$(document).ready(function() {
-		$('a[href^="#"]').click(function(){
-			var el = $(this).attr('href');
-			$('body').animate({ scrollTop: $(el).offset().top}, 800);
-			return false;
-			});
-	});
-
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("#callback").submit(function() { //Change
+	$("form").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
@@ -131,8 +62,3 @@ $(function() {
 	});
 
 });
-
-
-
-
-
